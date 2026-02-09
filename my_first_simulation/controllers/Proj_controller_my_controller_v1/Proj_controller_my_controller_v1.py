cfe5812197d3cc7eea0ae164ea9a52178a49fb87
matteoroda <matteo.roda@icloud.com>
2026-02-09 19:59:49 +0100

    Copied thr project robot and its controller to this project.

---
+++ my_first_simulation/controllers/Proj_controller_my_controller_v1/Proj_controller_my_controller_v1.py
@@ -0,0 +1,119 @@
+"""my_controller controller."""
+
+from controller import Robot 
+#Error due to library used by webots and not imported locally
+import numpy as np 
+import time
+
+DISTANCE_BETWEEN_WHEELS = 0.052 #(m) distance between wheels found in the robot manual
+WHEEL_RADIUS = 0.02 #(m) wheel radius found in the robot manual
+MAX_WHEEL_SPEED = 50 #(rad/s) actual max speed is 100, this setting is to not overspeed the robot
+
+# Function to set the wheel velocity
+def set_wheel_velocity(lin_vel, ang_vel):
+    # from set linear and angular velocity to wheel linear velocity
+    v_r = lin_vel + 0.5 * ang_vel * DISTANCE_BETWEEN_WHEELS
+    v_l = lin_vel - 0.5 * ang_vel * DISTANCE_BETWEEN_WHEELS
+
+    # linear -> angular (rad/s)
+    w_r = v_r / WHEEL_RADIUS
+    w_l = v_l / WHEEL_RADIUS
+
+    # saturation to preserve curvature
+    scale = max(abs(w_l), abs(w_r)) / MAX_WHEEL_SPEED
+
+    if scale > 1.0:
+        w_l /= scale
+        w_r /= scale
+
+    #print(f"Giving motor velocities: L: {w_l}, R: {w_r}")
+    
+    # set motor velocity
+    motorL.setVelocity(w_l)
+    motorR.setVelocity(w_r)
+
+# Function to get the current linear speed
+def get_current_linear_speed():
+    w_r = motorR.getVelocity()
+    w_l = motorL.getVelocity()
+    v_r = w_r * WHEEL_RADIUS
+    v_l = w_l * WHEEL_RADIUS
+    lin_vel = (v_r + v_l) / 2
+    return lin_vel
+
+# Function for linear acceleration
+def acc_speed(target_speed, current_speed, delta_time, time_to_target):
+    if current_speed <= target_speed:
+        delta_vel = (target_speed - current_speed) / delta_time
+    elif current_speed > target_speed:
+        delta_vel = (target_speed - current_speed) / delta_time
+    return current_speed + delta_vel*time_to_target
+
+# create the Robot instance
+robot = Robot()
+# get the time step of the current world
+timestep = int(robot.getBasicTimeStep()) #(ms) currently timestep is 1ms
+# Get motor devices
+motorL = robot.getDevice('left wheel motor')
+motorR = robot.getDevice('right wheel motor')
+
+# Setup distance sensor 
+ds1 = robot.getDevice('TestSensor1')
+# enable distance sensor with in order to have a good precision
+ds1.enable(timestep)
+
+
+# Set the motors to rotate indefinitely for velocity control
+motorL.setPosition(float('inf'))
+motorR.setPosition(float('inf'))
+
+# Variables for printing the sensor value every 1 second 
+last_print_time = 0.0
+
+# Variables for acceleration
+OBSTACLE_DECELERATION_TIME = 1 #(s)
+time_to_target = 0.0
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read and print the sensor value every 1 second 
+    current_time = robot.getTime()
+    dist = ds1.getValue()
+    if current_time - last_print_time >= 0.5:
+        print(f"The distance mesured by the distance sensor 1 at time {current_time}s is: {dist}")
+        last_print_time = current_time
+        # Get wheel speed and print it
+        w_r = motorR.getVelocity()
+        w_l = motorL.getVelocity()
+        print(f"Wheel speeds: L: {w_l}, R: {w_r}")
+
+    # set_wheel_velocity(0.1, 0)
+
+    # Simple obstacle avoidance with deceleration and acceleration
+    if dist < 750:
+        set_wheel_velocity(0.1, -5)
+        time_to_target = 0.0
+    elif dist < 1000: 
+        current_speed = get_current_linear_speed()
+        time_to_target += 0.010
+        new_speed = acc_speed(0.1, current_speed, OBSTACLE_DECELERATION_TIME, time_to_target)
+        set_wheel_velocity(new_speed, 0)
+    else:
+        current_speed = get_current_linear_speed()
+        if current_speed < 0.1:
+            time_to_target += 0.010
+            new_speed = acc_speed(0.1, current_speed, OBSTACLE_DECELERATION_TIME, time_to_target)
+            set_wheel_velocity(new_speed, 0)
+        else:
+            time_to_target = 0.0
+            set_wheel_velocity(0.1, 0)
+
+    pass
+
+# Enter here exit cleanup code.
+
+
+
+
+
